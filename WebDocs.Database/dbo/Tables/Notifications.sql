a70d3f50e5ac367963b0c967539da58831d3abc2
Brendan Wood <Brendanw@mweb.co.za>
2018-11-08 19:53:46 +0000

    DataBase

--- WebDocs.Database/dbo/Tables/Notifications.sql
+++ WebDocs.Database/dbo/Tables/Notifications.sql
@@ -1,19 +1,19 @@
 ﻿CREATE TABLE [dbo].[Notifications] (
-    [NotificationID]                INT           IDENTITY (1, 1) NOT NULL,
-    [FileID]                        INT           NOT NULL,
-    [NotificationTypeID]            INT           NOT NULL,
-    [DateCreated]                   DATETIME      NOT NULL,
-    [UserIDOfNotificationSender]    INT           NOT NULL,
-    [UserIDOfNotificationRecipient] INT           NOT NULL,
-    [NotificationSuccessfulSent]    BIT           CONSTRAINT [DF_Notifications_UserHasAcknowledgement] DEFAULT ((0)) NOT NULL,
-    [RetryAttempt]                  INT           CONSTRAINT [DF_Notifications_RetryAttempt] DEFAULT ((0)) NOT NULL,
-    [NotificationMessageTemplate]   VARCHAR (MAX) NOT NULL,
+    [NotificationID]                INT      IDENTITY (1, 1) NOT NULL,
+    [FileID]                        INT      NOT NULL,
+    [NotificationTypeID]            INT      NOT NULL,
+    [DateCreated]                   DATETIME NOT NULL,
+    [UserIDOfNotificationSender]    INT      NOT NULL,
+    [UserIDOfNotificationRecipient] INT      NOT NULL,
+    [UserHasAcknowledgement]        BIT      CONSTRAINT [DF_Notifications_UserHasAcknowledgement_1] DEFAULT ((0)) NOT NULL,
     CONSTRAINT [PK_Notifications] PRIMARY KEY CLUSTERED ([NotificationID] ASC),
     CONSTRAINT [FK_Notifications_AspNetUsers] FOREIGN KEY ([UserIDOfNotificationSender]) REFERENCES [dbo].[AspNetUsers] ([Id]),
     CONSTRAINT [FK_Notifications_AspNetUsers1] FOREIGN KEY ([UserIDOfNotificationRecipient]) REFERENCES [dbo].[AspNetUsers] ([Id]),
     CONSTRAINT [FK_Notifications_Files] FOREIGN KEY ([FileID]) REFERENCES [dbo].[Files] ([FileID]),
     CONSTRAINT [FK_Notifications_LookupTableNotificationTypes] FOREIGN KEY ([NotificationTypeID]) REFERENCES [dbo].[LookupTable_NotificationTypes] ([NotificationTypeID])
 );
+
+
 
 
 
